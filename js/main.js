511fee5a0061ac8775735761d4a67caa8ec8c253
Mark Muench <markmuench@u.boisestate.edu>
2024-10-19 20:59:26 -0600

    Brought Evil Circle to Program

--- js/main.js
+++ js/main.js
@@ -174,14 +174,23 @@ while (balls.length < 25) {
   balls.push(ball);
 }
 
+// Create the evil circle somewhere on the map
+const player = new EvilCircle(random(0,width), random(0,height));
+
 function loop() {
   ctx.fillStyle = "rgba(0, 0, 0, 0.25)";
   ctx.fillRect(0, 0, width, height);
 
   for (const ball of balls) {
-    ball.draw();
-    ball.update();
-    ball.collisionDetect();
+    if (ball.exists){
+      ball.draw();
+      ball.update();
+      ball.collisionDetect();
+    }
+
+    player.draw();
+    player.update();
+    player.collisionDetect();
   }
 
   requestAnimationFrame(loop);
